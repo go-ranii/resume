8ced922aebb2531f137318da7a5540018c7ec6e4
goranii <chanpoong@naver.com>
2022-04-18 16:40:02 +0900

    git resume#6

--- payload/profile.ts
+++ payload/profile.ts
@@ -1,7 +1,7 @@
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faBell } from '@fortawesome/free-regular-svg-icons';
 import { faGithub, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import image from '../asset/_sample.jpg';
+import image from '../asset/sample.jpg';
 import { IProfile } from '../component/profile/IProfile';
 
 const profile: IProfile.Payload = {
