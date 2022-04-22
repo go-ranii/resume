7777b1c1cb67c0fe828a86867926ce0e69fd88ed
goranii <chanpoong@naver.com>
2022-04-22 16:37:36 +0900

    :modify: resume#5

--- payload/_global.ts
+++ payload/_global.ts
@@ -2,8 +2,8 @@ import favicon from '../asset/favicon.ico';
 import previewImage from '../asset/preview.jpg';
 import { IGlobal } from '../component/common/IGlobal';
 
-const title = 'RESUME-NEXTJS Sample Page';
-const description = 'This is a simple web resume sample.';
+const title = 'Goranii RESUME';
+const description = 'Resume of goranii.';
 
 export const _global: IGlobal.Payload = {
   favicon,
@@ -24,10 +24,10 @@ export const _global: IGlobal.Payload = {
       ],
       type: 'profile',
       profile: {
-        firstName: 'Lorem',
-        lastName: 'ipsum',
-        username: 'lorem',
-        gender: 'female',
+        firstName: 'Jung Hwan',
+        lastName: 'Kim',
+        username: 'Goranii',
+        gender: 'male',
       },
     },
   },
